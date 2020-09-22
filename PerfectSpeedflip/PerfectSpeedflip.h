0ca98088b0bfd23bab7ea5470eb12efde009e7e0
ubelhj <ubelhj@uw.edu>
2020-09-22 11:01:38 -0700

    removes unused code from branch

--- PerfectSpeedflip/PerfectSpeedflip.h
+++ PerfectSpeedflip/PerfectSpeedflip.h
@@ -17,9 +17,6 @@ class PerfectSpeedflip: public BakkesMod::Plugin::BakkesModPlugin
 
 	void startKickoff();
 	void hitBall();
-	
-	// not used, part of failed speedflip trainer
-	void onTick();
 
 	// renders plugin
 	void render(CanvasWrapper canvas);
